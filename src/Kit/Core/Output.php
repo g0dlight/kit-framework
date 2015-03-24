ac42bf51c21fe15cedc74743fa8448bab66835fa
Orel Hadad <orelhadad@gmail.com>
2015-03-24 22:43:42 +0200

    fix Output

--- src/Kit/Core/Output.php
+++ src/Kit/Core/Output.php
@@ -6,31 +6,38 @@ use \Kit\Exception\KitException;
 
 final class Output{
 	private static $content = '';
+	private static $startObLevel = false;
 
 	public static function run(){
-		ob_start();
+		self::$startObLevel = ob_get_level();
 
+		ob_start();
 	}
 
 	public static function get(){
-		self::$content .= ob_get_contents();
+		if(ob_get_level() > self::$startObLevel){
+			self::$content .= ob_get_contents();
 
-		ob_clean();
+			ob_clean();
+		}
 
 		return self::$content;
 	}
 
 	public static function clean(){
-		self::$content = '';
+		if(ob_get_level() > self::$startObLevel)
+			ob_clean();
 
-		ob_clean();
+		self::$content = '';
 
 		return true;
 	}
 
 	public static function end(){
-		self::get();
-		ob_end_clean();
+		while(ob_get_level() > self::$startObLevel){
+			self::get();
+			ob_end_clean();
+		}
 	}
 
 	public static function flush(){
