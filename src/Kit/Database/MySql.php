1a3bb8e5d7610ba23093b95230ad88cccadcd029
Orel Hadad <orel@lok.team>
2016-03-22 02:58:43 +0200

    database init

---
+++ src/Kit/Database/MySql.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kit\Helpers\Database;
+
+use \PDO, \PDOException, \Exception;
+
+class MySql extends PDO{
+	public function __construct($user, $password = NULL, $server = 'localhost', $charset = 'utf8'){
+		try{
+			$dsn = 'mysql:host=' . $server . ';charset=' . $charset;
+			//dbname='.$database.';
+
+			parent::__construct($dsn, $user, $password);
+
+			return $this;
+		}
+		catch(PDOException $e){
+			throw new Exception('Connection failed: '.$e->getMessage());
+		}
+	}
+}
